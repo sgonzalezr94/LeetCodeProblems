f0e88abbce4b31e5e24a4635a7fafa20a925ff43
Sebastian Gonzalez R <sebastiang1493@gmail.com>
2022-12-04 10:52:26 -0500

    Completed second part of AoC Day 4 Challenge.

--- AdventOfCode2022/camp_cleanup.py
+++ AdventOfCode2022/camp_cleanup.py
@@ -22,6 +22,21 @@ class Solution:
         else:
             return False
 
+    # 2-6,4-8
+    def _is_overlapped(self, pairs_string: str) -> bool:
+        pair_1, pair_2 = pairs_string.split(",")
+        pair_1 = list(map(int, pair_1.split("-")))
+        pair_2 = list(map(int, pair_2.split("-")))
+        if (
+            (pair_2[0] <= pair_1[0] <= pair_2[1])
+            or (pair_2[0] <= pair_1[1] <= pair_2[1])
+            or (pair_1[0] <= pair_2[0] <= pair_1[1])
+            or (pair_1[0] <= pair_2[1] <= pair_1[1])
+        ):
+            return True
+        else:
+            return False
+
     def scan_contained_pairs(self, pairs_list: List[str]) -> int:
         if not pairs_list:
             contained_pairs = [
@@ -33,8 +48,23 @@ class Solution:
             ].count(True)
         return contained_pairs
 
+    def scan_overlapped_pairs(self, pairs_list: List[str]) -> int:
+        if not pairs_list:
+            over_lapped_pairs = [
+                self._is_overlapped(string_pair) for string_pair in self.list_of_pairs
+            ].count(True)
+        else:
+            over_lapped_pairs = [
+                self._is_overlapped(string_pair) for string_pair in self.list_of_pairs
+            ].count(True)
+        return over_lapped_pairs
+
 
 sol = Solution(4)
-ans = sol.scan_contained_pairs(sol.list_of_pairs)
+contained_ans = sol.scan_contained_pairs(sol.list_of_pairs)
+overlapped_ans = sol.scan_overlapped_pairs(sol.list_of_pairs)
 
+# 433
+# 852
 # That's the right answer! You are one gold star closer to collecting enough star fruit. Yay!
+# You have completed Day 4! You can [Share] this victory
